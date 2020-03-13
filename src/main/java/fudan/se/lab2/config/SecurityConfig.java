57fb3dc51d5179e150a9d2f1d169d631bef066e5
LeBW <27091925@qq.com>
2020-03-14 01:03:44 +0800

    add jwt parser

--- src/main/java/fudan/se/lab2/config/SecurityConfig.java
+++ src/main/java/fudan/se/lab2/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package fudan.se.lab2.config;
 
+import fudan.se.lab2.config.jwt.JwtRequestFilter;
 import fudan.se.lab2.service.JwtUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * @author LBW
@@ -20,31 +22,34 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     private JwtUserDetailsService userDetailsService;
+    private JwtRequestFilter jwtRequestFilter;
 
     @Autowired
-    public SecurityConfig(JwtUserDetailsService userDetailsService) {
+    public SecurityConfig(JwtUserDetailsService userDetailsService, JwtRequestFilter jwtRequestFilter) {
         this.userDetailsService = userDetailsService;
+        this.jwtRequestFilter = jwtRequestFilter;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(userDetailsService)
+        auth.userDetailsService(userDetailsService)
                 .passwordEncoder(new BCryptPasswordEncoder());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
+        // We dont't need CSRF for this project.
+        http.csrf().disable()
+                // Make sure we use stateless session; session won't be used to store user's state.
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
                 .antMatchers("/login", "/register", "/error").permitAll()
-                .anyRequest().authenticated()
-                .and();
-        //.addFilter(jwtRequestFilter);
+                .anyRequest().authenticated();
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
--- src/main/java/fudan/se/lab2/config/jwt/JwtTokenUtil.java
+++ src/main/java/fudan/se/lab2/config/jwt/JwtTokenUtil.java
@@ -2,14 +2,18 @@ package fudan.se.lab2.config.jwt;
 
 import fudan.se.lab2.config.SecurityConstant;
 import fudan.se.lab2.domain.User;
+import fudan.se.lab2.service.JwtUserDetailsService;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 /**
  * @author LBW
@@ -20,8 +24,8 @@ public class JwtTokenUtil implements Serializable {
     private static final long serialVersionUID = -3839549913040578986L;
 
     public static final long JWT_TOKEN_VALIDITY = SecurityConstant.EXPIRATION_TIME;
-    private String secret = SecurityConstant.SECRET;
-    private SignatureAlgorithm signatureAlgorithm = SecurityConstant.SIGNATURE_ALGORITHM;
+    private static final String secret = SecurityConstant.SECRET;
+    private static final SignatureAlgorithm signatureAlgorithm = SecurityConstant.SIGNATURE_ALGORITHM;
 
     public String generateToken(User user) {
         Map<String, Object> claims = new HashMap<>();
@@ -31,4 +35,35 @@ public class JwtTokenUtil implements Serializable {
                 .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY))
                 .signWith(signatureAlgorithm, secret).compact();
     }
+
+    public String getUsernameFromToken(String jwtToken) {
+        return getClaimFromToken(jwtToken, Claims::getSubject);
+    }
+
+    public boolean validateToken(String jwtToken, UserDetails userDetails) {
+        final String username = getUsernameFromToken(jwtToken);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(jwtToken));
+    }
+
+    private Date getExpirationDateFromToken(String jwtToken) {
+        return getClaimFromToken(jwtToken, Claims::getExpiration);
+    }
+
+    private <T> T getClaimFromToken(String jwtToken, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(jwtToken);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String jwtToken) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(jwtToken).getBody();
+    }
+
+
+
+    private boolean isTokenExpired(String jwtToken) {
+        final Date expiration = getExpirationDateFromToken(jwtToken);
+        return expiration.before(new Date());
+    }
+
+
 }
